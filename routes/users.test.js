3d43e1b81f9b38eeb5d17f4942ecb018f0d8660e
Tanmay Tripathi <tanmaytripathi348@gmail.com>
2022-06-14 19:53:49 +0530

    Car parking api

---
+++ routes/users.test.js
@@ -0,0 +1,8 @@
+import supertest from 'supertest';
+import users from './users.js';
+
+describe('Sample Test', () => {
+    it('should test that true === true', () => {
+      expect(true).toBe(true)
+    })
+  })
