f89aaec3bb3804ff52f879ab2b61f42e01d33e06
Tanmay Tripathi <tanmaytripathi348@gmail.com>
2022-06-14 19:56:14 +0530

    Deleting unwanted file

--- routes/users.test.js
+++
@@ -1,8 +0,0 @@
-import supertest from 'supertest';
-import users from './users.js';
-
-describe('Sample Test', () => {
-    it('should test that true === true', () => {
-      expect(true).toBe(true)
-    })
-  })
